51bfe8fc77d5e84051ef6f88bcd59a74d61006e8
Jeff Shen <jshen@flatironinstitute.org>
2026-02-13 15:03:32 -0500

    return model in freeze/unfreeze

--- lego/utils/__init__.py
+++ lego/utils/__init__.py
@@ -462,8 +462,9 @@ def module_wrap_policy(
 def freeze(model):
     for p in model.parameters():
         p.requires_grad = False
+    return model
 
 def unfreeze(model):
     for p in model.parameters():
         p.requires_grad = True
-
+    return model
